35e4f2fe007da0c994db9c331dd47121dbe3ccb8
Joe Ng <joe.ng@eguanatech.com>
2023-08-03 16:24:01 -0600

    test

--- source/logging/FileLogger.cpp
+++ source/logging/FileLogger.cpp
@@ -110,11 +110,17 @@ void FileLogger::run()
 
     while (!needsShutdown)
     {
-        auto now = std::chrono::system_clock::now();
-        std::time_t current_time = std::chrono::system_clock::to_time_t(now);
+        struct timespec timeSpec = {0, 0L}; /* Time structure       */
+        time_t retTime = 0;                 /* Time value to return */
+
+        if (clock_gettime(CLOCK_MONOTONIC, &(timeSpec)) == 0)
+        {
+            retTime = timeSpec.tv_sec;
+        }
+
         std::ofstream ofs;
         ofs.open("/var/run/watchdog.aws-iot-device-client", std::ofstream::out);
-        ofs << current_time << 1 << std::endl;
+        ofs << retTime << std::endl;
         ofs.close();
 
         unique_ptr<LogMessage> message = logQueue->getNextLog();
