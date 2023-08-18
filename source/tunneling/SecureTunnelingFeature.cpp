d2639e0fe16676ffe48d82e3909266a6e8b8e129
Joe Ng <joe.ng@eguanatech.com>
2023-08-18 14:49:44 -0600

    test

--- source/tunneling/SecureTunnelingFeature.cpp
+++ source/tunneling/SecureTunnelingFeature.cpp
@@ -183,7 +183,7 @@ namespace Aws
                             "by Device Client. region = %s, token = %s", response->Region->c_str(), response->ClientAccessToken->c_str());
                         string region = response->Region->c_str();
                         string ClientAccessToken = response->ClientAccessToken->c_str();
-                        string command = "localproxy -d SSH=10.3.2.1:22,GW=10.3.2.1:8080,TIVA=169.254.0.5:502 -r " + region + " -t " + ClientAccessToken;
+                        string command = "localproxy -d SSH=10.3.2.1:22,GW=10.3.2.1:8080,TIVA=169.254.0.5:502 -r " + region + " -t " + ClientAccessToken + " 2>&1 | tee /var/log/localproxy.log";
                         int ret = system(command.c_str());
                         LOGM_INFO(TAG, "Running localproxy instead return code = %d", ret);
                         return;
